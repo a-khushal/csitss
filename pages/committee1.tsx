c3f7a2ea4820a209292f7e884ef6b37192dbcf84
khushal <khushal2004a@gmail.com>
2024-06-22 23:50:34 +0530

    added data folder to contain all committee chairs data in different files

--- pages/committee1.tsx
+++ pages/committee1.tsx
@@ -5,7 +5,7 @@ export default function committee1() {
         <div className="grid grid-cols-1 lg:grid-cols-2 w-2/3 place-items-center">
             <div className="md:w-2/3 w-4/5 ">
                 <Card committeeName={"Conference Chairs"} to={"/conference_chairs"}/>
-                <Card committeeName={"Program Chairs"} to={""}/>
+                <Card committeeName={"Program Chairs"} to={"/program_chairs"}/>
                 <Card committeeName={"Technical Program Chairs"} to={""}/>
                 <Card committeeName={"Finance Chairs"} to={""}/>
                 <Card committeeName={"Publication Chairs"} to={""}/>
--- pages/program_chairs.tsx
+++ pages/program_chairs.tsx
@@ -1,10 +1,10 @@
 import { CommitteeCard } from "../components/CommitteeCard"
-import { conference_chairs_data } from "../data/conference_chairs"
+import { program_chairs_data } from "../data/program_chairs"
 
 export default function conferenceChairs() {
     return <div className="flex justify-center items-center">
         <div>
-            <CommitteeCard members={ conference_chairs_data }/>
+            <CommitteeCard members={ program_chairs_data }/>
         </div>
     </div>
 }
